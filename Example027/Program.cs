5681881fde1bb3d8dae9e1cee1c9d1c570e611af
Berus Frager <gitmailru@yandex.ru>
2022-08-16 22:51:47 +0300

    Коммит файлов папки проекта Example027

---
+++ Example027/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27:
+// Напишите программу, которая принимает на вход число и выдаёт
+// сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Sum = 0;
+Console.Write("Введите целое положительное число от 0 до 2 147 483 647: ");
+int NumberDigit = Convert.ToInt32(Console.ReadLine());
+int Number = NumberDigit;
+if (Number < 0 || Number > 2147483647)
+{
+    Console.WriteLine("Введенное число не соответствует условиям задачи.");
+}
+else
+{
+    while (NumberDigit > 0)
+    {
+        Sum  = Sum + NumberDigit % 10;
+        NumberDigit = NumberDigit / 10;
+    }
+}
+Console.WriteLine("Сумма всех цифр числа " + Number + " равна " + Sum);
