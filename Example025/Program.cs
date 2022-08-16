edd75d732de50daefbaa8b93d235b48af40f9cda
Berus Frager <gitmailru@yandex.ru>
2022-08-16 11:45:00 +0300

    Коммит файлов папки проекта Example025

---
+++ Example025/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25:
+// Напишите цикл, который принимает на вход два числа (A и B) и
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Clear();
+Console.Write("Введите целое число А, которое является основанием степени: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое число B, которое является показателем степени: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+int Degree = NumberA;
+if (NumberB == 0)
+{
+    Console.WriteLine(NumberA + " в степени 0 равно 1");
+}
+else
+{
+    for (int i = 1; i < NumberB; ++i)
+    {
+        Degree = Degree * NumberA;
+    }
+    Console.WriteLine(NumberA + " в степени " + NumberB + " равно " + Degree);
+}
