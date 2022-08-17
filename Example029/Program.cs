375daf3abd0596dcef52ff547a4ae423716b7efc
Berus Frager <gitmailru@yandex.ru>
2022-08-17 11:55:50 +0300

    Коммит файло папки проекта Example029

---
+++ Example029/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29:
+// Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+int[] DataBase = new int [8];
+int length = DataBase.Length;
+int index = 0;
+Console.Write("Элементы массива случайных чисел DataBase: [");
+for (index = 0; index < length; index++)
+{
+    DataBase[index] = new Random().Next(1, 100);
+    Console.Write(DataBase[index]);
+    if (index < 7)Console.Write(", ");
+}
+Console.Write("]");
